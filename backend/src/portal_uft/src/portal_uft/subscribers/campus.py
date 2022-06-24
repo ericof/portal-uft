0ac32a3eef36a6add81af305b8c98f266623d2bf
Érico Andrei <ericof@gmail.com>
2022-06-24 17:09:38 -0300

    Remove tags de Campus antes de adicionar as novas

--- backend/src/portal_uft/src/portal_uft/subscribers/campus.py
+++ backend/src/portal_uft/src/portal_uft/subscribers/campus.py
@@ -1,12 +1,13 @@
 from kitconcept import api
 from portal_uft.content.campus import Campus
 from zope.lifecycleevent import ObjectAddedEvent
+from zope.lifecycleevent import ObjectModifiedEvent
 
 
 def _update_tags(obj: Campus):
     """Update tags on Campus object."""
     vocab = api.vocabulary.get("portal_uft.vocabulary.cities")
-    tags = set(obj.subject)
+    tags = {tag for tag in obj.subject if not tag.startswith("Campus: ")}
     city = obj.city
     term = vocab.getTermByToken(city)
     tags.add(f"Campus: {term.title}")
