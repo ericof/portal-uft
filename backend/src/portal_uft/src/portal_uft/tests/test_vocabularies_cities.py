6f5189a0bbc1921e114aeb469001356a83c3a3f6
Érico Andrei <ericof@gmail.com>
2022-06-24 14:54:56 -0300

    Exercício 19

---
+++ backend/src/portal_uft/src/portal_uft/tests/test_vocabularies_cities.py
@@ -0,0 +1,28 @@
+from kitconcept import api
+from portal_uft.testing import PORTAL_UFT_INTEGRATION_TESTING
+
+import unittest
+
+
+VOCABULARY = "portal_uft.vocabulary.cities"
+
+
+class TestCitiesVocabulary(unittest.TestCase):
+
+    layer = PORTAL_UFT_INTEGRATION_TESTING
+
+    def setUp(self) -> None:
+        self.portal = self.layer["portal"]
+
+    def test_vocabulary(self):
+        vocab = api.vocabulary.get(VOCABULARY)
+        terms = [term for term in vocab]
+        self.assertEqual(len(terms), 3)
+
+    def test_vocabulary_titles(self):
+        vocab = api.vocabulary.get(VOCABULARY)
+        titles = [term.title for term in vocab]
+
+        self.assertIn("Palmas", titles)
+        self.assertIn("Araguaína", titles)
+        self.assertIn("Gurupi", titles)
---
+++ backend/src/portal_uft/src/portal_uft/vocabularies/cities.py
@@ -0,0 +1,20 @@
+from zope.interface import provider
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+
+
+CITIES = [
+    ["palmas", "Palmas"],
+    ["araguaina", "Araguaína"],
+    ["gurupi", "Gurupi"],
+]
+
+
+@provider(IVocabularyFactory)
+def cities_vocabulary(context):
+    """Vocabulary of cities in TO."""
+    terms = []
+    for token, title in CITIES:
+        terms.append(SimpleTerm(token, token, title))
+    return SimpleVocabulary(terms)
